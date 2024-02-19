7f7ce3261c31859908da6db95b841c2bf54f10d6
mateusw2705 <mateuswagner2705@gmail.com>
2024-02-19 08:18:26 -0300

    #

--- WepApi/Program.cs
+++ WepApi/Program.cs
@@ -85,36 +85,36 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-// JWT
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-      .AddJwtBearer(option =>
-      {
-          option.TokenValidationParameters = new TokenValidationParameters
-          {
-              ValidateIssuer = false,
-              ValidateAudience = false,
-              ValidateLifetime = true,
-              ValidateIssuerSigningKey = true,
+//// JWT
+//builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+//      .AddJwtBearer(option =>
+//      {
+//          option.TokenValidationParameters = new TokenValidationParameters
+//          {
+//              ValidateIssuer = false,
+//              ValidateAudience = false,
+//              ValidateLifetime = true,
+//              ValidateIssuerSigningKey = true,
 
-              ValidIssuer = "Teste.Securiry.Bearer",
-              ValidAudience = "Teste.Securiry.Bearer",
-              IssuerSigningKey = JwtSecurity.Create("Secret_Key-123456785kmfdshfdsnfdsnfuisdfsdf")
-          };
+//              ValidIssuer = "Teste.Securiry.Bearer",
+//              ValidAudience = "Teste.Securiry.Bearer",
+//              IssuerSigningKey = JwtSecurity.Create("Secret_Key-123456785kmfdshfdsnfdsnfuisdfsdf")
+//          };
 
-          option.Events = new JwtBearerEvents
-          {
-              OnAuthenticationFailed = context =>
-              {
-                  Console.WriteLine("OnAuthenticationFailed: " + context.Exception.Message);
-                  return Task.CompletedTask;
-              },
-              OnTokenValidated = context =>
-              {
-                  Console.WriteLine("OnTokenValidated: " + context.SecurityToken);
-                  return Task.CompletedTask;
-              }
-          };
-      });
+//          option.Events = new JwtBearerEvents
+//          {
+//              OnAuthenticationFailed = context =>
+//              {
+//                  Console.WriteLine("OnAuthenticationFailed: " + context.Exception.Message);
+//                  return Task.CompletedTask;
+//              },
+//              OnTokenValidated = context =>
+//              {
+//                  Console.WriteLine("OnTokenValidated: " + context.SecurityToken);
+//                  return Task.CompletedTask;
+//              }
+//          };
+//      });
 
 
 
